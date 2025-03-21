0eb91c8e89110fde3f76dc70e7bcb5cc13812d8d
IvdW <134393951+IvovdWerfDeltares@users.noreply.github.com>
2025-03-21 14:23:03 +0100

    Addition of Hudson 1959 for certain damage values, including tests

--- deltares_coastal_structures_toolbox/functions/structural/stability_rock_armour/hudson1959.py
+++ deltares_coastal_structures_toolbox/functions/structural/stability_rock_armour/hudson1959.py
@@ -109,3 +109,176 @@ def calculate_hudson1959_no_damage_Hs(
     Hs = (Delta / alpha_Hs) * ((M50 * KD * cot_alpha) / rho_armour) ** (1 / 3)
 
     return Hs
+
+
+def lookup_table_damage_factors(
+    damage_percentage: float | npt.NDArray[np.float64], rock_type: str = "rough"
+) -> float | npt.NDArray[np.float64]:
+    """Returns the multiplication value for the 0-damage value of the
+    hudson formula, according to SPM1984 and Rock Manual 2007/2012
+
+    Parameters
+    ----------
+    damage_percentage : float | npt.NDArray[np.float64]
+        Percentage of damage required
+    rock_type : str, optional
+        Type of rock, either "smooth" or "rough", by default "rough"
+
+    Returns
+    -------
+    float | npt.NDArray[np.float64]
+        Multiplication factor
+
+    Raises
+    ------
+    ValueError
+        Unknown rock_type
+    """
+
+    percentages = np.array([0, 5, 10, 15, 20, 30, 40, 50, 100])
+    mult_factors_rough = np.array([1.0, 1.0, 1.08, 1.14, 1.20, 1.29, 1.41, 1.54, 1.54])
+    mult_factors_smooth = np.array([1.0, 1.0, 1.08, 1.19, 1.27, 1.37, 1.47, 1.56, 1.56])
+
+    # if damage_percentage > percentages.max():
+    #     raise ValueError("Damage percentage higher then allowed value")
+
+    ind = np.searchsorted(percentages, damage_percentage, "right")
+    if rock_type.lower().strip() == "rough":
+        factor = mult_factors_rough[ind]
+    elif rock_type.lower().strip() == "smooth":
+        factor = mult_factors_smooth[ind]
+    else:
+        raise ValueError("Unknown rock type: {}".format(rock_type))
+
+    return factor
+
+
+lookup_table_damage_factors(60, "rough")
+
+
+def calculate_hudson1959_damage_M50(
+    Hs: float | npt.NDArray[np.float64],
+    rho_water: float | npt.NDArray[np.float64],
+    rho_armour: float | npt.NDArray[np.float64],
+    KD: float | npt.NDArray[np.float64],
+    cot_alpha: float | npt.NDArray[np.float64],
+    damage_percentage: float | npt.NDArray[np.float64],
+    rock_type: str = "rough",
+    alpha_Hs: float | npt.NDArray[np.float64] = 1.27,
+) -> float | npt.NDArray[np.float64]:
+    """Calculate the required M50 using the Hudson 1959 approach, including damage percentage
+
+    For more details see: Hudson 1959 and Rock Manual:
+    Hudson 1959, available here: https://doi.org/10.1061/JWHEAU.0000142 (or google)
+    or more information in the Rock Manual (2007 / 2012):
+    https://kennisbank-waterbouw.nl/DesignCodes/rockmanual/BWchapter%205.pdf page 564
+
+    Note: no limits to the formula have been provided in the paper
+
+    Parameters
+    ----------
+    Hs : float | npt.NDArray[np.float64]
+        Significant wave height (m)
+    rho_water : float | npt.NDArray[np.float64]
+        Water density (kg/m^3)
+    rho_armour : float | npt.NDArray[np.float64]
+        Armour rock density (kg/m^3)
+    KD : float | npt.NDArray[np.float64]
+        Stability coefficient, Hudson formula
+        Hints (Rock Manual, 2007) for use with H10Percent:
+        - KD = 2.0 for breaking waves
+        - KD = 4.0 for non-breaking waves
+        Breaking waves relate to breaking on foreshore, not to breaking on structure
+    cot_alpha : float | npt.NDArray[np.float64]
+        Cotangent of the front-side slope of the structure (-)
+    damage_percentage : float | npt.NDArray[np.float64]
+        Percentage of displaced units
+    alpha_Hs : float | npt.NDArray[np.float64], optional
+        Factor between Hs and H10Percent according to SPM, by default 1.27
+        (as per Hudson approach since Shore Protection Manual 1984)
+
+    Returns
+    -------
+    M50: float | npt.NDArray[np.float64]
+        Median rock mass (kg)
+    """
+
+    mult_factor = lookup_table_damage_factors(
+        damage_percentage=damage_percentage, rock_type=rock_type
+    )
+    M50 = calculate_hudson1959_no_damage_M50(
+        Hs=Hs,
+        rho_water=rho_water,
+        rho_armour=rho_armour,
+        KD=KD,
+        cot_alpha=cot_alpha,
+        alpha_Hs=alpha_Hs,
+    )
+
+    M50 = M50 / mult_factor**3
+
+    return M50
+
+
+def calculate_hudson1959_damage_Hs(
+    M50: float | npt.NDArray[np.float64],
+    rho_water: float | npt.NDArray[np.float64],
+    rho_armour: float | npt.NDArray[np.float64],
+    KD: float | npt.NDArray[np.float64],
+    cot_alpha: float | npt.NDArray[np.float64],
+    damage_percentage: float | npt.NDArray[np.float64],
+    rock_type: str = "rough",
+    alpha_Hs: float | npt.NDArray[np.float64] = 1.27,
+) -> float | npt.NDArray[np.float64]:
+    """Calculate the required Hs using the Hudson 1959 approach, including damage percentage
+
+    For more details see: Hudson 1959 and Rock Manual:
+    Hudson 1959, available here: https://doi.org/10.1061/JWHEAU.0000142 (or google)
+    or more information in the Rock Manual (2007 / 2012):
+    https://kennisbank-waterbouw.nl/DesignCodes/rockmanual/BWchapter%205.pdf page 564
+
+    Note: no limits to the formula have been provided in the paper
+
+    Parameters
+    ----------
+    M50 : float | npt.NDArray[np.float64]
+        Median rock mass (kg)
+    rho_water : float | npt.NDArray[np.float64]
+        Water density (kg/m^3)
+    rho_armour : float | npt.NDArray[np.float64]
+        Armour rock density (kg/m^3)
+    KD : float | npt.NDArray[np.float64]
+        Stability coefficient, Hudson formula
+        Hints (Rock Manual, 2007) for use with H10Percent:
+        - KD = 2.0 for breaking waves
+        - KD = 4.0 for non-breaking waves
+        Breaking waves relate to breaking on foreshore, not to breaking on structure
+    cot_alpha : float | npt.NDArray[np.float64]
+        Cotangent of the front-side slope of the structure (-)
+    damage_percentage : float | npt.NDArray[np.float64]
+        Percentage of displaced units
+    alpha_Hs : float | npt.NDArray[np.float64], optional
+        Factor between Hs and H10Percent according to SPM, by default 1.27
+        (as per Hudson approach since Shore Protection Manual 1984)
+
+    Returns
+    -------
+    Hs: float | npt.NDArray[np.float64]
+        Significant wave height (m)
+    """
+
+    mult_factor = lookup_table_damage_factors(
+        damage_percentage=damage_percentage, rock_type=rock_type
+    )
+    Hs = calculate_hudson1959_no_damage_Hs(
+        M50=M50,
+        rho_water=rho_water,
+        rho_armour=rho_armour,
+        KD=KD,
+        cot_alpha=cot_alpha,
+        alpha_Hs=alpha_Hs,
+    )
+
+    Hs = Hs * mult_factor
+
+    return Hs
--- tests/stability_hudson1959_test.py
+++ tests/stability_hudson1959_test.py
@@ -15,7 +15,7 @@ import deltares_coastal_structures_toolbox.functions.structural.stability_rock_a
         ([2.5, 1000, 2700, 2.0, 2.0, 1.0, 2146.7]),
     ),
 )
-def test_internal_consistency_Hs_M50(
+def test_internal_consistency_nodamage_Hs_M50(
     Hs,
     rho_water,
     rho_armour,
@@ -57,7 +57,7 @@ def test_internal_consistency_Hs_M50(
         ([2.5, 1000, 2700, 2.0, 2.0, 1.0, 2146.7]),
     ),
 )
-def test_M50_backward(
+def test_M50_nodamage_backward(
     Hs,
     rho_water,
     rho_armour,
@@ -90,7 +90,7 @@ def test_M50_backward(
         ([1900, 1000, 2700, 2.0, 2.0, 1.0, 2.40]),
     ),
 )
-def test_Hs_backward(
+def test_Hs_nodamage_backward(
     M50,
     rho_water,
     rho_armour,
@@ -109,3 +109,95 @@ def test_Hs_backward(
     )
 
     assert Hs_calculated == pytest.approx(Hs_expected, abs=1e-2)
+
+
+@pytest.mark.parametrize(
+    (
+        "Hs, rho_water, rho_armour, KD, cot_alpha, alpha_Hs, rock_type, damage_percentage, M50_expected"
+    ),
+    (
+        ([2.0, 1025, 2650, 4.0, 3.0, 1.27, "rough", 4, 908.19]),
+        ([2.0, 1025, 2650, 4.0, 3.0, 1.27, "rough", 5, 720.95]),
+        ([2.0, 1025, 2650, 4.0, 3.0, 1.27, "rough", 9, 720.95]),
+        ([2.0, 1025, 2650, 4.0, 3.0, 1.27, "rough", 10, 613.0]),
+        ([2.0, 1025, 2650, 4.0, 3.0, 1.27, "rough", 14, 613.0]),
+        ([2.0, 1025, 2650, 4.0, 3.0, 1.27, "rough", 15, 525.57]),
+        ([2.0, 1025, 2650, 4.0, 3.0, 1.27, "rough", 19, 525.57]),
+        ([2.0, 1025, 2650, 4.0, 3.0, 1.27, "rough", 20, 423.07]),
+        ([2.0, 1025, 2650, 4.0, 3.0, 1.27, "rough", 29, 423.07]),
+        ([2.0, 1025, 2650, 4.0, 3.0, 1.27, "rough", 30, 323.98]),
+        ([2.0, 1025, 2650, 4.0, 3.0, 1.27, "rough", 39, 323.98]),
+        ([2.0, 1025, 2650, 4.0, 3.0, 1.27, "rough", 40, 248.67]),
+        ([2.0, 1025, 2650, 4.0, 3.0, 1.27, "rough", 49, 248.67]),
+        # ([2.0, 1025, 2650, 4.0, 3.0, 1.27, "rough", 50, 248.67]),
+    ),
+)
+def test_M50_damage_backward(
+    Hs,
+    rho_water,
+    rho_armour,
+    KD,
+    cot_alpha,
+    alpha_Hs,
+    rock_type,
+    damage_percentage,
+    M50_expected,
+):
+    M50_calculated = hudson1959.calculate_hudson1959_damage_M50(
+        Hs=Hs,
+        rho_water=rho_water,
+        rho_armour=rho_armour,
+        KD=KD,
+        cot_alpha=cot_alpha,
+        alpha_Hs=alpha_Hs,
+        rock_type=rock_type,
+        damage_percentage=damage_percentage,
+    )
+
+    assert M50_calculated == pytest.approx(M50_expected, abs=1e-1)
+
+
+@pytest.mark.parametrize(
+    (
+        "M50, rho_water, rho_armour, KD, cot_alpha, alpha_Hs, rock_type, damage_percentage, Hs_expected"
+    ),
+    (
+        ([4500, 1025, 2650, 4.0, 3.0, 1.27, "rough", 4, 3.41]),
+        ([4500, 1025, 2650, 4.0, 3.0, 1.27, "rough", 5, 3.68]),
+        ([4500, 1025, 2650, 4.0, 3.0, 1.27, "rough", 9, 3.68]),
+        ([4500, 1025, 2650, 4.0, 3.0, 1.27, "rough", 10, 3.89]),
+        ([4500, 1025, 2650, 4.0, 3.0, 1.27, "rough", 14, 3.89]),
+        ([4500, 1025, 2650, 4.0, 3.0, 1.27, "rough", 15, 4.09]),
+        ([4500, 1025, 2650, 4.0, 3.0, 1.27, "rough", 19, 4.09]),
+        ([4500, 1025, 2650, 4.0, 3.0, 1.27, "rough", 20, 4.40]),
+        ([4500, 1025, 2650, 4.0, 3.0, 1.27, "rough", 29, 4.40]),
+        ([4500, 1025, 2650, 4.0, 3.0, 1.27, "rough", 30, 4.81]),
+        ([4500, 1025, 2650, 4.0, 3.0, 1.27, "rough", 39, 4.81]),
+        ([4500, 1025, 2650, 4.0, 3.0, 1.27, "rough", 40, 5.25]),
+        ([4500, 1025, 2650, 4.0, 3.0, 1.27, "rough", 49, 5.25]),
+        # ([2.0, 1025, 2650, 4.0, 3.0, 1.27, "rough", 50, 248.67]),
+    ),
+)
+def test_Hs_damage_backward(
+    M50,
+    rho_water,
+    rho_armour,
+    KD,
+    cot_alpha,
+    alpha_Hs,
+    rock_type,
+    damage_percentage,
+    Hs_expected,
+):
+    Hs_calculated = hudson1959.calculate_hudson1959_damage_Hs(
+        M50=M50,
+        rho_water=rho_water,
+        rho_armour=rho_armour,
+        KD=KD,
+        cot_alpha=cot_alpha,
+        alpha_Hs=alpha_Hs,
+        rock_type=rock_type,
+        damage_percentage=damage_percentage,
+    )
+
+    assert Hs_calculated == pytest.approx(Hs_expected, abs=1e-1)
