f339329dfd6b9747113351974fb68df8df1b3d1a
IvdW <134393951+IvovdWerfDeltares@users.noreply.github.com>
2025-03-24 20:15:40 +0100

    Addition of coreloc + tests

---
+++ tests/stability_coreloc_hudson1959_test.py
@@ -0,0 +1,67 @@
+import pytest
+
+import deltares_coastal_structures_toolbox.functions.structural.stability_concrete_armour.core_loc_Hudson1959 as hudson1959
+
+
+@pytest.mark.parametrize(
+    ("Hs, rho_water, rho_armour, KD, cot_alpha, alpha_Hs, M_expected"),
+    (
+        ([3.0, 1025, 2400, 16.0, 1.33, 1.0, 1261.4]),
+        ([3.0, 1000, 2400, 16.0, 1.33, 1.0, 1109.7]),
+        ([3.0, 1025, 2600, 16.0, 1.33, 1.0, 909.28]),
+        ([3.0, 1025, 2400, 13.0, 1.33, 1.0, 1552.5]),
+        ([3.0, 1025, 2400, 16.0, 1.5, 1.0, 1118.5]),
+        ([5.0, 1025, 2400, 16.0, 1.33, 1.0, 5840.0]),
+    ),
+)
+def test_M_nodamage_backward(
+    Hs,
+    rho_water,
+    rho_armour,
+    KD,
+    cot_alpha,
+    alpha_Hs,
+    M_expected,
+):
+    M_calculated = hudson1959.calculate_unit_mass_M_Hudson1959(
+        Hs=Hs,
+        rho_water=rho_water,
+        rho_armour=rho_armour,
+        KD=KD,
+        cot_alpha=cot_alpha,
+        alpha_Hs=alpha_Hs,
+    )
+
+    assert M_calculated == pytest.approx(M_expected, abs=1e-1)
+
+
+@pytest.mark.parametrize(
+    ("M, rho_water, rho_armour, KD, cot_alpha, alpha_Hs, Hs_expected"),
+    (
+        ([3000, 1025, 2400, 16.0, 1.33, 1.0, 4.00]),
+        ([6000, 1025, 2400, 16.0, 1.33, 1.0, 5.05]),
+        ([3000, 1000, 2400, 16.0, 1.33, 1.0, 4.18]),
+        ([3000, 1025, 2600, 16.0, 1.33, 1.0, 4.47]),
+        ([3000, 1025, 2400, 13.0, 1.33, 1.0, 3.74]),
+        ([3000, 1025, 2400, 16.0, 1.5, 1.0, 4.17]),
+    ),
+)
+def test_Hs_nodamage_backward(
+    M,
+    rho_water,
+    rho_armour,
+    KD,
+    cot_alpha,
+    alpha_Hs,
+    Hs_expected,
+):
+    Hs_calculated = hudson1959.calculate_significant_wave_height_Hs_Hudson1959(
+        M=M,
+        rho_water=rho_water,
+        rho_armour=rho_armour,
+        KD=KD,
+        cot_alpha=cot_alpha,
+        alpha_Hs=alpha_Hs,
+    )
+
+    assert Hs_calculated == pytest.approx(Hs_expected, abs=1e-2)
