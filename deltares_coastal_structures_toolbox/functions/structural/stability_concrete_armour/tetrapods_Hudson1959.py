8e92227292a95611134b5a57a2f032c4dc7114ba
IvdW <134393951+IvovdWerfDeltares@users.noreply.github.com>
2025-03-24 16:34:13 +0100

    Fix formatting issues > unused imports

--- deltares_coastal_structures_toolbox/functions/structural/stability_concrete_armour/tetrapods_Hudson1959.py
+++ deltares_coastal_structures_toolbox/functions/structural/stability_concrete_armour/tetrapods_Hudson1959.py
@@ -2,9 +2,6 @@
 import numpy as np
 import numpy.typing as npt
 
-import deltares_coastal_structures_toolbox.functions.core_physics as core_physics
-
-# import deltares_coastal_structures_toolbox.functions.core_utility as core_utility
 import deltares_coastal_structures_toolbox.functions.structural.stability_rock_armour.hudson1959 as hudson
 
 unit_properties = {
