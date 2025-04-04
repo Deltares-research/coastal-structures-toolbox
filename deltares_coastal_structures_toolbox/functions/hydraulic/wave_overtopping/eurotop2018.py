32f48877138c34895a6d6e9aa05ed8a33b51ad96
Joost den Bieman <jpdenbieman@gmail.com>
2025-04-04 10:37:21 +0200

    Update eurotop2018.py
    implement rubble mound variant formulas

--- deltares_coastal_structures_toolbox/functions/hydraulic/wave_overtopping/eurotop2018.py
+++ deltares_coastal_structures_toolbox/functions/hydraulic/wave_overtopping/eurotop2018.py
@@ -3,8 +3,25 @@ import numpy as np
 import numpy.typing as npt
 
 import deltares_coastal_structures_toolbox.functions.core_physics as core_physics
+import deltares_coastal_structures_toolbox.functions.core_utility as core_utility
 import deltares_coastal_structures_toolbox.functions.hydraulic.wave_runup.eurotop2018 as wave_runup_eurotop2018
 import deltares_coastal_structures_toolbox.functions.hydraulic.wave_runup.taw2002 as wave_runup_taw2002
+
+
+def check_validity_range_rubble_mound(
+    cot_alpha: float | npt.NDArray[np.float64] = np.nan,
+) -> None:
+
+    if not np.any(np.isnan(cot_alpha)):
+        core_utility.check_variable_validity_range(
+            "Cotangent alpha",
+            "EurOtop (2018) - rubble mound",
+            cot_alpha,
+            4.0 / 3.0,
+            2.0,
+        )
+
+    return
 
 
 def calculate_overtopping_discharge_q(
@@ -645,3 +662,224 @@ def calculate_dimensionless_crest_freeboard(
     max_reached = np.min([Rc_diml_eq510, Rc_diml_eq511], axis=0) == Rc_diml_eq511
 
     return Rc_diml, max_reached
+
+
+def calculate_overtopping_discharge_q_rubble_mound(
+    Hm0: float | npt.NDArray[np.float64],
+    Tmm10: float | npt.NDArray[np.float64],
+    Rc: float | npt.NDArray[np.float64],
+    beta: float | npt.NDArray[np.float64] = np.nan,
+    gamma_beta: float | npt.NDArray[np.float64] = np.nan,
+    gamma_f: float | npt.NDArray[np.float64] = 1.0,
+    B_berm: float | npt.NDArray[np.float64] = 0.0,
+    db: float | npt.NDArray[np.float64] = 0.0,
+    cot_alpha: float | npt.NDArray[np.float64] = np.nan,
+    cot_alpha_down: float | npt.NDArray[np.float64] = np.nan,
+    cot_alpha_up: float | npt.NDArray[np.float64] = np.nan,
+    c2: float = 0.1035,
+    c3: float = 1.35,
+    use_best_fit: bool = False,
+    g: float = 9.81,
+) -> float | npt.NDArray[np.float64]:
+
+    q_diml = calculate_dimensionless_overtopping_discharge_q_rubble_mound(
+        Hm0=Hm0,
+        Tmm10=Tmm10,
+        beta=beta,
+        gamma_beta=gamma_beta,
+        cot_alpha=cot_alpha,
+        cot_alpha_down=cot_alpha_down,
+        cot_alpha_up=cot_alpha_up,
+        Rc=Rc,
+        B_berm=B_berm,
+        db=db,
+        gamma_f=gamma_f,
+        c2=c2,
+        c3=c3,
+        use_best_fit=use_best_fit,
+    )
+    q = q_diml * np.sqrt(g * Hm0**3)
+
+    return q
+
+
+def calculate_dimensionless_overtopping_discharge_q_rubble_mound(
+    Hm0: float | npt.NDArray[np.float64],
+    Tmm10: float | npt.NDArray[np.float64],
+    Rc: float | npt.NDArray[np.float64],
+    beta: float | npt.NDArray[np.float64] = np.nan,
+    gamma_beta: float | npt.NDArray[np.float64] = np.nan,
+    gamma_f: float | npt.NDArray[np.float64] = 1.0,
+    B_berm: float | npt.NDArray[np.float64] = 0.0,
+    db: float | npt.NDArray[np.float64] = 0.0,
+    cot_alpha: float | npt.NDArray[np.float64] = np.nan,
+    cot_alpha_down: float | npt.NDArray[np.float64] = np.nan,
+    cot_alpha_up: float | npt.NDArray[np.float64] = np.nan,
+    c2: float = 0.1035,
+    c3: float = 1.35,
+    use_best_fit: bool = False,
+) -> float | npt.NDArray[np.float64]:
+
+    _, c2, c3, _ = check_best_fit(
+        c1=np.nan, c2=c2, c3=c3, c4=np.nan, use_best_fit=use_best_fit
+    )
+
+    if wave_runup_taw2002.check_calculate_gamma_beta(beta=beta, gamma_beta=gamma_beta):
+        gamma_beta = calculate_influence_oblique_waves_gamma_beta(
+            beta=beta, gamma_f=gamma_f
+        )
+
+    if wave_runup_taw2002.check_composite_slope(
+        cot_alpha=cot_alpha, cot_alpha_down=cot_alpha_down, cot_alpha_up=cot_alpha_up
+    ):
+        z2p_for_slope = wave_runup_eurotop2018.iteration_procedure_z2p(
+            Hm0=Hm0,
+            Tmm10=Tmm10,
+            cot_alpha_down=cot_alpha_down,
+            cot_alpha_up=cot_alpha_up,
+            B_berm=B_berm,
+            db=db,
+            gamma_f=gamma_f,
+            gamma_beta=gamma_beta,
+            use_best_fit=use_best_fit,
+        )
+
+        cot_alpha = wave_runup_taw2002.determine_average_slope(
+            Hm0=Hm0,
+            z2p=z2p_for_slope,
+            cot_alpha_down=cot_alpha_down,
+            cot_alpha_up=cot_alpha_up,
+            B_berm=B_berm,
+            db=db,
+        )
+
+    ksi_mm10 = core_physics.calculate_Irribarren_number_ksi(
+        H=Hm0, T=Tmm10, cot_alpha=cot_alpha
+    )
+
+    gamma_f_adj = wave_runup_taw2002.calculate_adjusted_influence_roughness_gamma_f(
+        gamma_f=gamma_f, gamma_b=1.0, ksi_mm10=ksi_mm10
+    )
+
+    q_diml = c2 * np.exp(
+        -1.0 * np.power(c3 * (Rc / Hm0) * (1.0 / (gamma_f_adj * gamma_beta)), 1.3)
+    )
+
+    check_validity_range_rubble_mound(
+        cot_alpha=cot_alpha,
+    )
+
+    return q_diml
+
+
+def calculate_crest_freeboard_Rc_rubble_mound(
+    Hm0: float | npt.NDArray[np.float64],
+    Tmm10: float | npt.NDArray[np.float64],
+    q: float | npt.NDArray[np.float64],
+    beta: float | npt.NDArray[np.float64] = np.nan,
+    gamma_beta: float | npt.NDArray[np.float64] = np.nan,
+    gamma_f: float | npt.NDArray[np.float64] = 1.0,
+    B_berm: float | npt.NDArray[np.float64] = 0.0,
+    db: float | npt.NDArray[np.float64] = 0.0,
+    cot_alpha: float | npt.NDArray[np.float64] = np.nan,
+    cot_alpha_down: float | npt.NDArray[np.float64] = np.nan,
+    cot_alpha_up: float | npt.NDArray[np.float64] = np.nan,
+    c2: float = 0.1035,
+    c3: float = 1.35,
+    use_best_fit: bool = False,
+    g: float = 9.81,
+) -> float | npt.NDArray[np.float64]:
+
+    Rc_diml, max_reached = calculate_dimensionless_crest_freeboard_rubble_mound(
+        Hm0=Hm0,
+        Tmm10=Tmm10,
+        beta=beta,
+        gamma_beta=gamma_beta,
+        cot_alpha=cot_alpha,
+        cot_alpha_down=cot_alpha_down,
+        cot_alpha_up=cot_alpha_up,
+        q=q,
+        B_berm=B_berm,
+        db=db,
+        gamma_f=gamma_f,
+        c2=c2,
+        c3=c3,
+        use_best_fit=use_best_fit,
+        g=g,
+    )
+
+    Rc = Rc_diml * Hm0
+
+    return Rc, max_reached
+
+
+def calculate_dimensionless_crest_freeboard_rubble_mound(
+    Hm0: float | npt.NDArray[np.float64],
+    Tmm10: float | npt.NDArray[np.float64],
+    q: float | npt.NDArray[np.float64],
+    beta: float | npt.NDArray[np.float64] = np.nan,
+    gamma_beta: float | npt.NDArray[np.float64] = np.nan,
+    gamma_f: float | npt.NDArray[np.float64] = 1.0,
+    B_berm: float | npt.NDArray[np.float64] = 0.0,
+    db: float | npt.NDArray[np.float64] = 0.0,
+    cot_alpha: float | npt.NDArray[np.float64] = np.nan,
+    cot_alpha_down: float | npt.NDArray[np.float64] = np.nan,
+    cot_alpha_up: float | npt.NDArray[np.float64] = np.nan,
+    c2: float = 0.1035,
+    c3: float = 1.35,
+    use_best_fit: bool = False,
+    g: float = 9.81,
+) -> tuple[float | npt.NDArray[np.float64], bool | npt.NDArray[np.bool]]:
+
+    _, c2, c3, _ = check_best_fit(
+        c1=np.nan, c2=c2, c3=c3, c4=np.nan, use_best_fit=use_best_fit
+    )
+
+    if wave_runup_taw2002.check_calculate_gamma_beta(beta=beta, gamma_beta=gamma_beta):
+        gamma_beta = calculate_influence_oblique_waves_gamma_beta(
+            beta=beta, gamma_f=gamma_f
+        )
+
+    if wave_runup_taw2002.check_composite_slope(
+        cot_alpha=cot_alpha, cot_alpha_down=cot_alpha_down, cot_alpha_up=cot_alpha_up
+    ):
+        z2p_for_slope = wave_runup_eurotop2018.iteration_procedure_z2p(
+            Hm0=Hm0,
+            Tmm10=Tmm10,
+            cot_alpha_down=cot_alpha_down,
+            cot_alpha_up=cot_alpha_up,
+            B_berm=B_berm,
+            db=db,
+            gamma_f=gamma_f,
+            gamma_beta=gamma_beta,
+        )
+
+        cot_alpha = wave_runup_taw2002.determine_average_slope(
+            Hm0=Hm0,
+            z2p=z2p_for_slope,
+            cot_alpha_down=cot_alpha_down,
+            cot_alpha_up=cot_alpha_up,
+            B_berm=B_berm,
+            db=db,
+        )
+
+    ksi_mm10 = core_physics.calculate_Irribarren_number_ksi(
+        H=Hm0, T=Tmm10, cot_alpha=cot_alpha
+    )
+
+    gamma_f_adj = wave_runup_taw2002.calculate_adjusted_influence_roughness_gamma_f(
+        gamma_f=gamma_f, gamma_b=1.0, ksi_mm10=ksi_mm10
+    )
+
+    Rc_diml = (
+        np.power(-np.log((q / np.sqrt(g * np.power(Hm0, 3))) * (1.0 / c2)), 1.0 / 1.3)
+        * (1.0 / c3)
+        * gamma_f_adj
+        * gamma_beta
+    )
+
+    check_validity_range_rubble_mound(
+        cot_alpha=cot_alpha,
+    )
+
+    return Rc_diml
