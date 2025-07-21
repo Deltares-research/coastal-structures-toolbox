0f513da65a2108d420ff157ea9aee57a4b8990cb
Joost den Bieman <jpdenbieman@gmail.com>
2025-07-21 13:37:35 +0200

    Include Rc calculation

--- deltares_coastal_structures_toolbox/functions/hydraulic/wave_overtopping/vangent2025.py
+++ deltares_coastal_structures_toolbox/functions/hydraulic/wave_overtopping/vangent2025.py
@@ -630,200 +630,333 @@ def calculate_influence_wind_gamma_w(
     return gamma_w
 
 
-# def calculate_crest_freeboard_Rc(
-#     Hm0: float | npt.NDArray[np.float64],
-#     Tmm10: float | npt.NDArray[np.float64],
-#     q: float | npt.NDArray[np.float64],
-#     cot_alpha: float | npt.NDArray[np.float64] = np.nan,
-#     cot_alpha_down: float | npt.NDArray[np.float64] = np.nan,
-#     cot_alpha_up: float | npt.NDArray[np.float64] = np.nan,
-#     beta: float | npt.NDArray[np.float64] = np.nan,
-#     B_berm: float | npt.NDArray[np.float64] = 0.0,
-#     db: float | npt.NDArray[np.float64] = 0.0,
-#     gamma_beta: float | npt.NDArray[np.float64] = np.nan,
-#     gamma_b: float | npt.NDArray[np.float64] = np.nan,
-#     gamma_f: float | npt.NDArray[np.float64] = 1.0,
-#     gamma_v: float | npt.NDArray[np.float64] = 1.0,
-#     sigma: float | npt.NDArray[np.float64] = 0,
-#     g: float = 9.81,
-#     use_best_fit: bool = False,
-# ) -> tuple[float | npt.NDArray[np.float64], bool | npt.NDArray[np.bool]]:
+def calculate_crest_freeboard_Rc(
+    Hm0: float | npt.NDArray[np.float64],
+    Hm0_swell: float | npt.NDArray[np.float64],
+    Tmm10: float | npt.NDArray[np.float64],
+    q: float | npt.NDArray[np.float64],
+    Ac: float | npt.NDArray[np.float64],
+    cot_alpha: float | npt.NDArray[np.float64] = np.nan,
+    cot_alpha_down: float | npt.NDArray[np.float64] = np.nan,
+    cot_alpha_up: float | npt.NDArray[np.float64] = np.nan,
+    beta: float | npt.NDArray[np.float64] = np.nan,
+    B_berm: float | npt.NDArray[np.float64] = 0.0,
+    db: float | npt.NDArray[np.float64] = 0.0,
+    Dn50: float | npt.NDArray[np.float64] = np.nan,
+    gamma_f: float | npt.NDArray[np.float64] = np.nan,
+    gamma_b: float | npt.NDArray[np.float64] = np.nan,
+    gamma_v: float | npt.NDArray[np.float64] = np.nan,
+    gamma_beta: float | npt.NDArray[np.float64] = np.nan,
+    design_calculation: bool = True,
+    include_influence_wind: bool = False,
+) -> tuple[float | npt.NDArray[np.float64], bool | npt.NDArray[np.bool]]:
+    """_summary_
 
-#     Rc_diml, max_reached = calculate_dimensionless_crest_freeboard(
-#         Hm0=Hm0,
-#         Tmm10=Tmm10,
-#         beta=beta,
-#         gamma_beta=gamma_beta,
-#         cot_alpha=cot_alpha,
-#         cot_alpha_down=cot_alpha_down,
-#         cot_alpha_up=cot_alpha_up,
-#         q=q,
-#         B_berm=B_berm,
-#         db=db,
-#         gamma_b=gamma_b,
-#         gamma_f=gamma_f,
-#         gamma_v=gamma_v,
-#         sigma=sigma,
-#         g=g,
-#         use_best_fit=use_best_fit,
-#     )
+    _extended_summary_
 
-#     Rc = Rc_diml * Hm0
+    Parameters
+    ----------
+    Hm0 : float | npt.NDArray[np.float64]
+        _description_
+    Hm0_swell : float | npt.NDArray[np.float64]
+        _description_
+    Tmm10 : float | npt.NDArray[np.float64]
+        _description_
+    q : float | npt.NDArray[np.float64]
+        _description_
+    Ac : float | npt.NDArray[np.float64]
+        _description_
+    cot_alpha : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    cot_alpha_down : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    cot_alpha_up : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    beta : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    B_berm : float | npt.NDArray[np.float64], optional
+        _description_, by default 0.0
+    db : float | npt.NDArray[np.float64], optional
+        _description_, by default 0.0
+    Dn50 : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    gamma_f : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    gamma_b : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    gamma_v : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    gamma_beta : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    design_calculation : bool, optional
+        _description_, by default True
+    include_influence_wind : bool, optional
+        _description_, by default False
 
-#     return Rc, max_reached
+    Returns
+    -------
+    tuple[float | npt.NDArray[np.float64], bool | npt.NDArray[np.bool]]
+        _description_
+    """
 
+    Rc_diml, max_reached = calculate_dimensionless_crest_freeboard(
+        Hm0=Hm0,
+        Hm0_swell=Hm0_swell,
+        Tmm10=Tmm10,
+        q=q,
+        Ac=Ac,
+        cot_alpha=cot_alpha,
+        cot_alpha_down=cot_alpha_down,
+        cot_alpha_up=cot_alpha_up,
+        beta=beta,
+        B_berm=B_berm,
+        db=db,
+        Dn50=Dn50,
+        gamma_f=gamma_f,
+        gamma_b=gamma_b,
+        gamma_v=gamma_v,
+        gamma_beta=gamma_beta,
+        design_calculation=design_calculation,
+        include_influence_wind=include_influence_wind,
+    )
 
-# def calculate_dimensionless_crest_freeboard(
-#     Hm0: float | npt.NDArray[np.float64],
-#     Tmm10: float | npt.NDArray[np.float64],
-#     q: float | npt.NDArray[np.float64],
-#     cot_alpha: float | npt.NDArray[np.float64] = np.nan,
-#     cot_alpha_down: float | npt.NDArray[np.float64] = np.nan,
-#     cot_alpha_up: float | npt.NDArray[np.float64] = np.nan,
-#     beta: float | npt.NDArray[np.float64] = np.nan,
-#     B_berm: float | npt.NDArray[np.float64] = 0.0,
-#     db: float | npt.NDArray[np.float64] = 0.0,
-#     gamma_beta: float | npt.NDArray[np.float64] = np.nan,
-#     gamma_b: float | npt.NDArray[np.float64] = np.nan,
-#     gamma_f: float | npt.NDArray[np.float64] = 1.0,
-#     gamma_v: float | npt.NDArray[np.float64] = 1.0,
-#     sigma: float | npt.NDArray[np.float64] = 0,
-#     g: float = 9.81,
-#     use_best_fit: bool = False,
-# ) -> tuple[float | npt.NDArray[np.float64], bool | npt.NDArray[np.bool]]:
+    Rc = Rc_diml * Hm0
 
-#     if use_best_fit:
-#         c1 = 4.75
-#         c3 = 2.6
-#     else:
-#         c1 = 4.3
-#         c3 = 2.3
-
-#     if sigma == 0:
-#         cor1 = 0
-#         cor3 = 0
-#     else:
-#         if use_best_fit:
-#             cor1 = 0.5 * sigma
-#             cor3 = 0.35 * sigma
-#         else:
-#             warnings.warn(
-#                 (
-#                     "Sigma is only applicable to the best fit coefficients! The design values of the coefficients "
-#                     "alreaddy account for uncertainty with conservative coefficient values."
-#                 )
-#             )
-
-#     if wave_runup_taw2002.check_calculate_gamma_beta(beta=beta, gamma_beta=gamma_beta):
-#         gamma_beta = calculate_influence_oblique_waves_gamma_beta(beta=beta)
-
-#     if wave_runup_taw2002.check_composite_slope(
-#         cot_alpha=cot_alpha, cot_alpha_down=cot_alpha_down, cot_alpha_up=cot_alpha_up
-#     ):
-#         z2p_for_slope = wave_runup_taw2002.iteration_procedure_z2p(
-#             Hm0=Hm0,
-#             Tmm10=Tmm10,
-#             cot_alpha_down=cot_alpha_down,
-#             cot_alpha_up=cot_alpha_up,
-#             B_berm=B_berm,
-#             db=db,
-#             gamma_f=gamma_f,
-#             gamma_beta=gamma_beta,
-#         )
-
-#         cot_alpha = wave_runup_taw2002.determine_average_slope(
-#             Hm0=Hm0,
-#             z2p=z2p_for_slope,
-#             cot_alpha_down=cot_alpha_down,
-#             cot_alpha_up=cot_alpha_up,
-#             B_berm=B_berm,
-#             db=db,
-#         )
-
-#     ksi_mm10 = core_physics.calculate_Irribarren_number_ksi(
-#         H=Hm0, T=Tmm10, cot_alpha=cot_alpha
-#     )
-
-#     if np.isnan(gamma_b):
-#         L_berm = wave_runup_taw2002.calculate_berm_length(
-#             Hm0=Hm0,
-#             cot_alpha=cot_alpha,
-#             cot_alpha_down=cot_alpha_down,
-#             cot_alpha_up=cot_alpha_up,
-#             B_berm=B_berm,
-#         )
-
-#         gamma_b = wave_runup_taw2002.iteration_procedure_gamma_b(
-#             Hm0=Hm0,
-#             Tmm10=Tmm10,
-#             cot_alpha_average=cot_alpha,
-#             B_berm=B_berm,
-#             L_berm=L_berm,
-#             db=db,
-#             gamma_f=gamma_f,
-#             gamma_beta=gamma_beta,
-#         )
-
-#     gamma_f_adj = wave_runup_taw2002.calculate_adjusted_influence_roughness_gamma_f(
-#         gamma_f=gamma_f, gamma_b=gamma_b, ksi_mm10=ksi_mm10
-#     )
-
-#     Rc_diml_eq24 = (
-#         np.log(
-#             (1.0 / 0.067)
-#             * np.sqrt(1.0 / cot_alpha)
-#             * (1.0 / gamma_b)
-#             * (1.0 / ksi_mm10)
-#             * q
-#             / np.sqrt(g * Hm0**3)
-#         )
-#         * (-1.0 / (c1 + cor1))
-#         * ksi_mm10
-#         * gamma_b
-#         * gamma_f_adj
-#         * gamma_beta
-#         * gamma_v
-#     )
-
-#     Rc_diml_max = Rc_diml_max_equation(
-#         Hm0=Hm0, q=q, c3=c3, cor3=cor3, gamma_beta=gamma_beta, gamma_f=gamma_f_adj
-#     )
-
-#     Rc_diml = np.min([Rc_diml_eq24, Rc_diml_max], axis=0)
-#     max_reached = np.min([Rc_diml_eq24, Rc_diml_max], axis=0) == Rc_diml_max
-
-#     check_validity_range(
-#         Hm0=Hm0,
-#         Tmm10=Tmm10,
-#         beta=beta,
-#         cot_alpha=cot_alpha,
-#         cot_alpha_down=cot_alpha_down,
-#         cot_alpha_up=cot_alpha_up,
-#         gamma_f=gamma_f,
-#         gamma_b=gamma_b,
-#         gamma_beta=gamma_beta,
-#         gamma_v=gamma_v,
-#     )
-
-#     return Rc_diml, max_reached
+    return Rc, max_reached
 
 
-# def Rc_diml_max_equation(
-#     Hm0: float | npt.NDArray[np.float64],
-#     q: float | npt.NDArray[np.float64],
-#     gamma_beta: float | npt.NDArray[np.float64],
-#     gamma_f: float | npt.NDArray[np.float64],
-#     c3: float,
-#     cor3: float = 0.0,
-#     c2: float = 0.2,
-#     g: float = 9.81,
-# ):
+def calculate_dimensionless_crest_freeboard(
+    Hm0: float | npt.NDArray[np.float64],
+    Hm0_swell: float | npt.NDArray[np.float64],
+    Tmm10: float | npt.NDArray[np.float64],
+    q: float | npt.NDArray[np.float64],
+    Ac: float | npt.NDArray[np.float64],
+    cot_alpha: float | npt.NDArray[np.float64] = np.nan,
+    cot_alpha_down: float | npt.NDArray[np.float64] = np.nan,
+    cot_alpha_up: float | npt.NDArray[np.float64] = np.nan,
+    beta: float | npt.NDArray[np.float64] = np.nan,
+    B_berm: float | npt.NDArray[np.float64] = 0.0,
+    db: float | npt.NDArray[np.float64] = 0.0,
+    Dn50: float | npt.NDArray[np.float64] = np.nan,
+    gamma_f: float | npt.NDArray[np.float64] = np.nan,
+    gamma_b: float | npt.NDArray[np.float64] = np.nan,
+    gamma_v: float | npt.NDArray[np.float64] = np.nan,
+    gamma_beta: float | npt.NDArray[np.float64] = np.nan,
+    g: float = 9.81,
+    design_calculation: bool = True,
+    include_influence_wind: bool = False,
+) -> tuple[float | npt.NDArray[np.float64], bool | npt.NDArray[np.bool]]:
+    """_summary_
 
-#     Rc_diml_max = (
-#         np.log((1.0 / c2) * q / np.sqrt(g * Hm0**3))
-#         * (-1.0 / (c3 + cor3))
-#         * gamma_f
-#         * gamma_beta
-#     )
+    _extended_summary_
 
-#     return Rc_diml_max
+    Parameters
+    ----------
+    Hm0 : float | npt.NDArray[np.float64]
+        _description_
+    Hm0_swell : float | npt.NDArray[np.float64]
+        _description_
+    Tmm10 : float | npt.NDArray[np.float64]
+        _description_
+    q : float | npt.NDArray[np.float64]
+        _description_
+    Ac : float | npt.NDArray[np.float64]
+        _description_
+    cot_alpha : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    cot_alpha_down : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    cot_alpha_up : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    beta : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    B_berm : float | npt.NDArray[np.float64], optional
+        _description_, by default 0.0
+    db : float | npt.NDArray[np.float64], optional
+        _description_, by default 0.0
+    Dn50 : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    gamma_f : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    gamma_b : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    gamma_v : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    gamma_beta : float | npt.NDArray[np.float64], optional
+        _description_, by default np.nan
+    g : float, optional
+        _description_, by default 9.81
+    design_calculation : bool, optional
+        _description_, by default True
+    include_influence_wind : bool, optional
+        _description_, by default False
+
+    Returns
+    -------
+    tuple[float | npt.NDArray[np.float64], bool | npt.NDArray[np.bool]]
+        _description_
+
+    Raises
+    ------
+    ValueError
+        _description_
+    """
+
+    if np.isnan(gamma_beta):
+        gamma_beta = calculate_influence_oblique_waves_gamma_beta(beta=beta)
+
+    # TODO check with Marcel how to handle composite slopes (maybe irrelevant for RMB)
+    if wave_runup_taw2002.check_composite_slope(
+        cot_alpha=cot_alpha, cot_alpha_down=cot_alpha_down, cot_alpha_up=cot_alpha_up
+    ):
+        z2p_for_slope = wave_runup_taw2002.iteration_procedure_z2p(
+            Hm0=Hm0,
+            Tmm10=Tmm10,
+            cot_alpha_down=cot_alpha_down,
+            cot_alpha_up=cot_alpha_up,
+            B_berm=B_berm,
+            db=db,
+            gamma_f=gamma_f,
+            gamma_beta=gamma_beta,
+        )
+
+        cot_alpha = wave_runup_taw2002.determine_average_slope(
+            Hm0=Hm0,
+            z2p=z2p_for_slope,
+            cot_alpha_down=cot_alpha_down,
+            cot_alpha_up=cot_alpha_up,
+            B_berm=B_berm,
+            db=db,
+        )
+
+    ksi_mm10 = core_physics.calculate_Irribarren_number_ksi(
+        H=Hm0, T=Tmm10, cot_alpha=cot_alpha
+    )
+
+    if np.isnan(gamma_b):
+        gamma_b = calculate_influence_berm_gamma_b(
+            Hm0=Hm0,
+            smm10=ksi_mm10,
+            Ac=Ac,
+            B_berm=B_berm,
+            BL=Ac - db,
+        )
+
+    if np.isnan(gamma_f):
+        if np.isnan(Dn50):
+            raise ValueError("Either gamma_f or Dn50 should be provided")
+        smm10 = core_physics.calculate_wave_steepness_s(Hm0, Tmm10)
+        gamma_f = calculate_influence_friction_gamma_f(Dn50=Dn50, Hm0=Hm0, smm10=smm10)
+
+    # TODO Check with Marcel: how to handle wind influence and design conservatism in Rc?
+    Rc_diml = (
+        np.log((1.0 / 6.8) * cot_alpha * q / np.sqrt(g * Hm0**3))
+        * (-1.0 / 5.0)
+        * gamma_f
+        * gamma_b
+        * gamma_v
+        * gamma_beta
+        * ksi_mm10
+        - 0.4 * Hm0_swell / Hm0
+    )
+
+    Rc_diml_max = Rc_diml_max_equation(
+        Hm0=Hm0,
+        Hm0_swell=Hm0_swell,
+        q=q,
+        cot_alpha=cot_alpha,
+        ksi_mm10=ksi_mm10,
+        gamma_f=gamma_f,
+        gamma_b=gamma_b,
+        gamma_v=gamma_v,
+        gamma_beta=gamma_beta,
+    )
+
+    Rc_diml = np.min([Rc_diml, Rc_diml_max], axis=0)
+    max_reached = np.min([Rc_diml, Rc_diml_max], axis=0) == Rc_diml_max
+
+    if include_influence_wind:
+        print("Please implement wind influence calculation.")
+
+    if design_calculation:
+        print("Please implement design calculation.")
+
+    check_validity_range(
+        Hm0=Hm0,
+        Tmm10=Tmm10,
+        beta=beta,
+        cot_alpha=cot_alpha,
+        cot_alpha_down=cot_alpha_down,
+        cot_alpha_up=cot_alpha_up,
+        gamma_f=gamma_f,
+        gamma_b=gamma_b,
+        gamma_beta=gamma_beta,
+        gamma_v=gamma_v,
+    )
+
+    return Rc_diml, max_reached
+
+
+def Rc_diml_max_equation(
+    Hm0: float | npt.NDArray[np.float64],
+    Hm0_swell: float | npt.NDArray[np.float64],
+    q: float | npt.NDArray[np.float64],
+    cot_alpha: float | npt.NDArray[np.float64],
+    ksi_mm10: float | npt.NDArray[np.float64],
+    gamma_f: float | npt.NDArray[np.float64],
+    gamma_b: float | npt.NDArray[np.float64],
+    gamma_v: float | npt.NDArray[np.float64],
+    gamma_beta: float | npt.NDArray[np.float64],
+    c2: float = 0.8,
+    c3: float = -2.5,
+    g: float = 9.81,
+):
+    """_summary_
+
+    _extended_summary_
+
+    Parameters
+    ----------
+    Hm0 : float | npt.NDArray[np.float64]
+        _description_
+    Hm0_swell : float | npt.NDArray[np.float64]
+        _description_
+    q : float | npt.NDArray[np.float64]
+        _description_
+    cot_alpha : float | npt.NDArray[np.float64]
+        _description_
+    ksi_mm10 : float | npt.NDArray[np.float64]
+        _description_
+    gamma_f : float | npt.NDArray[np.float64]
+        _description_
+    gamma_b : float | npt.NDArray[np.float64]
+        _description_
+    gamma_v : float | npt.NDArray[np.float64]
+        _description_
+    gamma_beta : float | npt.NDArray[np.float64]
+        _description_
+    c2 : float, optional
+        _description_, by default 0.8
+    c3 : float, optional
+        _description_, by default -2.5
+    g : float, optional
+        _description_, by default 9.81
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
+
+    Rc_diml_max = (
+        np.log((1.0 / c2) * cot_alpha * q / np.sqrt(g * Hm0**3))
+        * (1.0 / c3)
+        * gamma_f
+        * gamma_b
+        * gamma_v
+        * gamma_beta
+        * np.power(ksi_mm10, 0.24)
+        - 0.4 * Hm0_swell / Hm0
+    )
+
+    return Rc_diml_max
