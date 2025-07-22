8fd0971782b291e46a1111323589bf96f0cf44a9
Joost den Bieman <jpdenbieman@gmail.com>
2025-07-22 17:23:10 +0200

    Include iterative solution for Hs

--- tests/structural/stability_rock_armour/jumelet2024_test.py
+++ tests/structural/stability_rock_armour/jumelet2024_test.py
@@ -69,43 +69,37 @@ def test_Dn50_backward(
     assert Dn50_calculated == pytest.approx(Dn50_expected, abs=1e-2)
 
 
-# @pytest.mark.parametrize(
-#     ("cot_alpha, P, rho_armour, N_waves, Tmm10, S, M50, Hs_expected"),
-#     (
-#         ([3.0, 0.4, 2650, 3000, 6.0, 2.0, 1000.0, 2.36]),
-#         ([2.0, 0.4, 2650, 3000, 6.0, 2.0, 1000.0, 1.80]),
-#         ([3.0, 0.5, 2650, 3000, 6.0, 2.0, 1000.0, 2.49]),
-#         ([3.0, 0.4, 2850, 3000, 6.0, 2.0, 1000.0, 2.66]),
-#         ([3.0, 0.4, 2650, 6000, 6.0, 2.0, 1000.0, 2.15]),
-#         ([3.0, 0.4, 2650, 3000, 14.0, 2.0, 1000.0, 1.91]),
-#         ([3.0, 0.4, 2650, 3000, 6.0, 3.0, 1000.0, 2.63]),
-#         ([3.0, 0.4, 2650, 3000, 6.0, 2.0, 1200.0, 2.56]),
-#         ([4.0, 0.4, 2650, 3000, 6.0, 2.0, 1000.0, 2.85]),
-#         ([4.0, 0.4, 2650, 3000, 14.0, 2.0, 1000.0, 1.62]),
-#     ),
-# )
-# def test_Hs_backward(
-#     cot_alpha,
-#     P,
-#     rho_armour,
-#     N_waves,
-#     Tmm10,
-#     S,
-#     M50,
-#     Hs_expected,
-# ):
-#     Hs_calculated = jumelet2024.calculate_significant_wave_height_Hs(
-#         ratio_H2p_Hs=1.4,
-#         Tmm10=Tmm10,
-#         cot_alpha=cot_alpha,
-#         rho_armour=rho_armour,
-#         S=S,
-#         P=P,
-#         N_waves=N_waves,
-#         M50=M50,
-#     )
+@pytest.mark.parametrize(
+    ("cot_alpha, rho_armour, N_waves, Tmm10, S, M50, Hs_expected"),
+    (
+        ([8.0, 2650, 3000, 6.0, 2.0, 150.0, 2.14]),
+        ([7.0, 2650, 3000, 6.0, 2.0, 150.0, 1.59]),
+        ([8.0, 2850, 3000, 6.0, 2.0, 150.0, 2.62]),
+        ([8.0, 2650, 6000, 6.0, 2.0, 150.0, 1.85]),
+        ([8.0, 2650, 3000, 8.0, 2.0, 150.0, 1.18]),
+        ([8.0, 2650, 3000, 6.0, 3.0, 150.0, 2.56]),
+        ([8.0, 2650, 3000, 6.0, 2.0, 100.0, 1.60]),
+    ),
+)
+def test_Hs_backward(
+    cot_alpha,
+    rho_armour,
+    N_waves,
+    Tmm10,
+    S,
+    M50,
+    Hs_expected,
+):
+    Hs_calculated = jumelet2024.calculate_significant_wave_height_Hs(
+        Tmm10=Tmm10,
+        cot_alpha=cot_alpha,
+        rho_armour=rho_armour,
+        S=S,
+        N_waves=N_waves,
+        M50=M50,
+    )
 
-#     assert Hs_calculated == pytest.approx(Hs_expected, abs=1e-2)
+    assert Hs_calculated == pytest.approx(Hs_expected, abs=1e-2)
 
 
 @pytest.mark.parametrize(
@@ -150,97 +144,83 @@ def test_internal_consistency_S_Dn50(
     assert S_calculated == pytest.approx(S, abs=1e-2)
 
 
-# @pytest.mark.parametrize(
-#     ("cot_alpha, P, rho_armour, N_waves, Tmm10, Hs, S"),
-#     (
-#         ([3.0, 0.4, 2650, 3000, 6.0, 2.0, 2.0]),
-#         ([2.0, 0.4, 2650, 3000, 6.0, 2.0, 2.0]),
-#         ([3.0, 0.5, 2650, 3000, 6.0, 2.0, 2.0]),
-#         ([3.0, 0.4, 2850, 3000, 6.0, 2.0, 2.0]),
-#         ([3.0, 0.4, 2650, 6000, 6.0, 2.0, 2.0]),
-#         ([3.0, 0.4, 2650, 3000, 14.0, 2.0, 2.0]),
-#         ([3.0, 0.4, 2650, 3000, 6.0, 2.5, 2.0]),
-#         ([3.0, 0.4, 2650, 3000, 6.0, 2.0, 3.0]),
-#         ([3.6, 0.4, 2650, 3000, 14.0, 2.0, 2.0]),
-#     ),
-# )
-# def test_internal_consistency_Hs_Dn50(
-#     cot_alpha,
-#     P,
-#     rho_armour,
-#     N_waves,
-#     Tmm10,
-#     Hs,
-#     S,
-# ):
-#     Dn50_calculated = jumelet2024.calculate_nominal_rock_diameter_Dn50(
-#         Hs=Hs,
-#         H2p=1.4 * Hs,
-#         Tmm10=Tmm10,
-#         cot_alpha=cot_alpha,
-#         rho_armour=rho_armour,
-#         P=P,
-#         N_waves=N_waves,
-#         S=S,
-#     )
+@pytest.mark.parametrize(
+    ("cot_alpha, rho_armour, N_waves, Tmm10, Hs, S"),
+    (
+        ([8.0, 2650, 3000, 6.0, 2.0, 2.0]),
+        ([7.0, 2650, 3000, 6.0, 2.0, 2.0]),
+        ([8.0, 2850, 3000, 6.0, 2.0, 2.0]),
+        ([8.0, 2650, 6000, 6.0, 2.0, 2.0]),
+        ([8.0, 2650, 3000, 10.0, 2.0, 2.0]),
+        ([8.0, 2650, 3000, 6.0, 2.5, 2.0]),
+        ([8.0, 2650, 3000, 6.0, 2.0, 3.0]),
+    ),
+)
+def test_internal_consistency_Hs_Dn50(
+    cot_alpha,
+    rho_armour,
+    N_waves,
+    Tmm10,
+    Hs,
+    S,
+):
+    Dn50_calculated = jumelet2024.calculate_nominal_rock_diameter_Dn50(
+        Hs=Hs,
+        Tmm10=Tmm10,
+        cot_alpha=cot_alpha,
+        rho_armour=rho_armour,
+        N_waves=N_waves,
+        S=S,
+    )
 
-#     Hs_calculated = jumelet2024.calculate_significant_wave_height_Hs(
-#         ratio_H2p_Hs=1.4,
-#         Tmm10=Tmm10,
-#         cot_alpha=cot_alpha,
-#         rho_armour=rho_armour,
-#         S=S,
-#         P=P,
-#         N_waves=N_waves,
-#         Dn50=Dn50_calculated,
-#     )
+    Hs_calculated = jumelet2024.calculate_significant_wave_height_Hs(
+        Tmm10=Tmm10,
+        cot_alpha=cot_alpha,
+        rho_armour=rho_armour,
+        S=S,
+        N_waves=N_waves,
+        Dn50=Dn50_calculated,
+    )
 
-#     assert Hs_calculated == pytest.approx(Hs, abs=1e-2)
+    assert Hs_calculated == pytest.approx(Hs, abs=1e-2)
 
 
-# @pytest.mark.parametrize(
-#     ("cot_alpha, P, rho_armour, N_waves, Tmm10, Hs, M50"),
-#     (
-#         ([3.0, 0.4, 2650, 3000, 6.0, 2.0, 1000.0]),
-#         ([2.0, 0.4, 2650, 3000, 6.0, 2.0, 1000.0]),
-#         ([3.0, 0.5, 2650, 3000, 6.0, 2.0, 1000.0]),
-#         ([3.0, 0.4, 2850, 3000, 6.0, 2.0, 1000.0]),
-#         ([3.0, 0.4, 2650, 6000, 6.0, 2.0, 1000.0]),
-#         ([3.0, 0.4, 2650, 3000, 14.0, 2.0, 1000.0]),
-#         ([3.0, 0.4, 2650, 3000, 6.0, 2.5, 1000.0]),
-#         ([3.0, 0.4, 2650, 3000, 6.0, 2.0, 1200.0]),
-#         ([3.6, 0.4, 2650, 3000, 14.0, 2.0, 1000.0]),
-#     ),
-# )
-# def test_internal_consistency_S_Hs(
-#     cot_alpha,
-#     P,
-#     rho_armour,
-#     N_waves,
-#     Tmm10,
-#     Hs,
-#     M50,
-# ):
-#     S_calculated = jumelet2024.calculate_damage_number_S(
-#         Hs=Hs,
-#         H2p=1.4 * Hs,
-#         Tmm10=Tmm10,
-#         cot_alpha=cot_alpha,
-#         rho_armour=rho_armour,
-#         P=P,
-#         N_waves=N_waves,
-#         M50=M50,
-#     )
+@pytest.mark.parametrize(
+    ("cot_alpha, rho_armour, N_waves, Tmm10, Hs, M50"),
+    (
+        ([8.0, 2650, 3000, 6.0, 2.0, 150.0]),
+        ([7.0, 2650, 3000, 6.0, 2.0, 150.0]),
+        ([8.0, 2850, 3000, 6.0, 2.0, 150.0]),
+        ([8.0, 2650, 6000, 6.0, 2.0, 150.0]),
+        ([8.0, 2650, 3000, 10.0, 2.0, 150.0]),
+        ([8.0, 2650, 3000, 6.0, 2.5, 150.0]),
+        ([8.0, 2650, 3000, 6.0, 2.0, 100.0]),
+    ),
+)
+def test_internal_consistency_S_Hs(
+    cot_alpha,
+    rho_armour,
+    N_waves,
+    Tmm10,
+    Hs,
+    M50,
+):
+    S_calculated = jumelet2024.calculate_damage_number_S(
+        Hs=Hs,
+        Tmm10=Tmm10,
+        cot_alpha=cot_alpha,
+        rho_armour=rho_armour,
+        N_waves=N_waves,
+        M50=M50,
+    )
 
-#     Hs_calculated = jumelet2024.calculate_significant_wave_height_Hs(
-#         ratio_H2p_Hs=1.4,
-#         Tmm10=Tmm10,
-#         cot_alpha=cot_alpha,
-#         rho_armour=rho_armour,
-#         S=S_calculated,
-#         P=P,
-#         N_waves=N_waves,
-#         M50=M50,
-#     )
+    Hs_calculated = jumelet2024.calculate_significant_wave_height_Hs(
+        Tmm10=Tmm10,
+        cot_alpha=cot_alpha,
+        rho_armour=rho_armour,
+        S=S_calculated,
+        N_waves=N_waves,
+        M50=M50,
+    )
 
-#     assert Hs_calculated == pytest.approx(Hs, abs=1e-2)
+    assert Hs_calculated == pytest.approx(Hs, abs=1e-2)
