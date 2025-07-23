b62b1e0fb8823d0b32c9f1a0ecac5c930ad7869c
Joost den Bieman <jpdenbieman@gmail.com>
2025-07-23 17:09:05 +0200

    Update denbieman2021.py

--- deltares_coastal_structures_toolbox/functions/hydraulic/wave_overtopping/denbieman2021.py
+++ deltares_coastal_structures_toolbox/functions/hydraulic/wave_overtopping/denbieman2021.py
@@ -26,7 +26,59 @@ def write_input_file_XGB_Overtopping(
     output_dir: str,
     file_name: str = "input_XGB_Overtopping",
 ):
-    # TODO check equal lengths (or length = 1, then copy everything to the same length)
+    """Generate input file for XGB Overtopping model described by Den Bieman et al. (2021).
+
+    This function creates a CSV input file with the necessary parameters for the XGB Overtopping model, which
+    predicts the mean wave overtopping discharge q (m^3/s/m) including uncertainties. The model is available
+    as a free web tool at https://www.deltares.nl/en/software-and-data/products/overtopping-xgb.
+
+    The input file can contain up to 200 combinations of input parameters.
+
+    For more details, see Den Bieman et al. (2021), which is available here:
+    https://doi.org/10.1016/j.coastaleng.2020.103830
+    or here:
+    https://www.researchgate.net/publication/346963461_Wave_overtopping_predictions_using_an_advanced_machine_learning_technique
+
+    Parameters
+    ----------
+    beta : float | npt.NDArray[np.float64]
+        Angle of wave incidence (degrees)
+    h : float | npt.NDArray[np.float64]
+        Water depth at toe of the structure (m)
+    Hm0 : float | npt.NDArray[np.float64]
+        Spectral significant wave height (m)
+    Tmm10 : float | npt.NDArray[np.float64]
+        Spectral wave period Tm-1,0 (s)
+    Bt : float | npt.NDArray[np.float64]
+        Toe width of the structure (m)
+    ht : float | npt.NDArray[np.float64]
+        Water depth above the toe of the structure (m)
+    B_berm : float | npt.NDArray[np.float64]
+        Berm width of the structure (m)
+    db : float | npt.NDArray[np.float64]
+        Berm height of the structure (m)
+    Rc : float | npt.NDArray[np.float64]
+        Crest freeboard of the structure (m)
+    Ac : float | npt.NDArray[np.float64]
+        Armour crest freeboard of the structure (m)
+    Gc : float | npt.NDArray[np.float64]
+        Width of the crest of the structure (m)
+    cot_alpha_down : float | npt.NDArray[np.float64]
+        Cotangent of the lower part of the front-side slope of the structure (-)
+    cot_alpha_up : float | npt.NDArray[np.float64]
+        Cotangent of the lower part of the front-side slope of the structure (-)
+    gamma_f_down : float | npt.NDArray[np.float64]
+        Influence factor for surface roughness of the lower part of the structure (-)
+    gamma_f_up : float | npt.NDArray[np.float64]
+        Influence factor for surface roughness of the upper part of the structure (-)
+    tan_alpha_f : float | npt.NDArray[np.float64]
+        Tangent of the foreshore slope (-).
+    output_dir : str
+        Directory where the input file will be saved.
+    file_name : str, optional
+        Name of the input file, by default "input_XGB_Overtopping"
+    """
+
     if isinstance(beta, float):
         index = [0]
     else:
