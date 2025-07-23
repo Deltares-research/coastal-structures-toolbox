dda6a5f6de519f04723fac62905b01b966d832dc
Joost den Bieman <jpdenbieman@gmail.com>
2025-07-23 13:33:44 +0200

    WIP generator for input files XGB-OT

---
+++ deltares_coastal_structures_toolbox/functions/hydraulic/wave_overtopping/denbieman2021.py
@@ -0,0 +1,57 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+import os
+
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+
+import deltares_coastal_structures_toolbox.functions.core_physics as core_physics
+import deltares_coastal_structures_toolbox.functions.core_utility as core_utility
+import deltares_coastal_structures_toolbox.functions.hydraulic.wave_runup.taw2002 as wave_runup_taw2002
+
+
+def write_input_file_XGB_Overtopping(
+    beta: float | npt.NDArray[np.float64],
+    h: float | npt.NDArray[np.float64],
+    Hm0: float | npt.NDArray[np.float64],
+    Tmm10: float | npt.NDArray[np.float64],
+    Bt: float | npt.NDArray[np.float64],
+    ht: float | npt.NDArray[np.float64],
+    B_berm: float | npt.NDArray[np.float64],
+    db: float | npt.NDArray[np.float64],
+    Rc: float | npt.NDArray[np.float64],
+    Ac: float | npt.NDArray[np.float64],
+    Gc: float | npt.NDArray[np.float64],
+    cot_alpha_down: float | npt.NDArray[np.float64],
+    cot_alpha_up: float | npt.NDArray[np.float64],
+    gamma_f_down: float | npt.NDArray[np.float64],
+    gamma_f_up: float | npt.NDArray[np.float64],
+    tan_alpha_f: float | npt.NDArray[np.float64],
+    output_dir: str,
+    file_name: str = "input_XGB_Overtopping",
+):
+    # TODO check equal lengths (or length = 1, then copy everything to the same length)
+
+    input_dataframe = pd.DataFrame(
+        data={
+            "b": beta,
+            "h": h,
+            "Hm0 toe": Hm0,
+            "Tm-1,0 toe": Tmm10,
+            "ht": ht,
+            "Bt": Bt,
+            "hb": db,
+            "B": B_berm,
+            "Rc": Rc,
+            "Ac": Ac,
+            "Gc": Gc,
+            "cotad": cot_alpha_down,
+            "cotau": cot_alpha_up,
+            "gf_d": gamma_f_down,
+            "gf_u": gamma_f_up,
+            "tanaf": tan_alpha_f,
+        }
+    )
+    input_dataframe.to_csv(os.path.join(output_dir, f"{file_name}.csv"), sep=";")
+
+    return
