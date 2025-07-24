c91b9101eeac49723045ed5c3583ddb1e249a957
Joost den Bieman <jpdenbieman@gmail.com>
2025-07-24 10:37:53 +0200

    Update vangent2007.py

--- deltares_coastal_structures_toolbox/functions/hydraulic/wave_overtopping/vangent2007.py
+++ deltares_coastal_structures_toolbox/functions/hydraulic/wave_overtopping/vangent2007.py
@@ -31,7 +31,8 @@ def write_input_file_NN_Overtopping(
     predicts the mean wave overtopping discharge q (m^3/s/m) including uncertainties. The model is available
     as a free web tool at https://www.deltares.nl/en/software-and-data/products/overtopping-neural-network.
 
-    All input parameters should be (1D) arrays of the same length.
+    The input file can contain up to 200 combinations of input parameters. All input parameters should be
+    (1D) arrays of the same length.
 
     Note that the NN Overtopping model is also available as a downloadable installer here:
     https://dserie.deltares.nl/NNOvertopping/helppage.aspx
