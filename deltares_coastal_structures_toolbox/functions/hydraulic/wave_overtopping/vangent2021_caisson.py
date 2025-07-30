8ce2a75f3787a4260e9e03d5868d52591fa329e8
Joost den Bieman <jpdenbieman@gmail.com>
2025-07-30 11:40:37 +0200

    Implement Rc calculation

--- deltares_coastal_structures_toolbox/functions/hydraulic/wave_overtopping/vangent2021_caisson.py
+++ deltares_coastal_structures_toolbox/functions/hydraulic/wave_overtopping/vangent2021_caisson.py
@@ -93,6 +93,150 @@ def calculate_dimensionless_overtopping_discharge_q(
     """Calculate the dimensionless mean wave overtopping discharge q for caisson
     breakwaters with the Van Gent (2021) formula."""
 
+    a, b, c_beta, gamma_p = _determine_coefficients(
+        c=c,
+        short_crested_waves=short_crested_waves,
+        crossing_seas=crossing_seas,
+        parapet=parapet,
+    )
+
+    if not crossing_seas:
+        c_swell = 0.0
+    else:
+        if np.isnan(Hm0_swell):
+            raise ValueError(
+                "Hm0_swell must be provided when calculating wave overtopping for crossing seas."
+            )
+
+    if np.isnan(gamma_beta):
+        gamma_beta = calculate_influence_oblique_waves_gamma_beta(
+            beta=beta,
+            gamma_p=gamma_p,
+            c_beta=c_beta,
+        )
+
+    q_diml = a * np.exp(
+        -(b / (gamma_beta * gamma_p)) * np.power((Rc - c_swell * Hm0_swell) / Hm0, c)
+    )
+
+    check_validity_range(
+        Hm0=Hm0,
+        beta=beta,
+        Rc=Rc,
+        q_diml=q_diml,
+    )
+
+    return q_diml
+
+
+def calculate_influence_oblique_waves_gamma_beta(
+    beta: float | npt.NDArray[np.float64],
+    gamma_p: float,
+    c_beta: float,
+) -> float | npt.NDArray[np.float64]:
+    # """Calculate the influence factor for oblique wave incidence gamma_beta"""
+
+    gamma_beta = (1 - c_beta / gamma_p) * np.power(
+        np.cos(np.radians(beta)), 2
+    ) + c_beta / gamma_p
+
+    return gamma_beta
+
+
+def calculate_crest_freeboard_Rc(
+    Hm0: float | npt.NDArray[np.float64],
+    q: float | npt.NDArray[np.float64],
+    beta: float | npt.NDArray[np.float64] = np.nan,
+    gamma_beta: float | npt.NDArray[np.float64] = np.nan,
+    Hm0_swell: float | npt.NDArray[np.float64] = np.nan,
+    c: float = 1.0,
+    c_swell: float = 0.4,
+    short_crested_waves: bool = True,
+    crossing_seas: bool = False,
+    parapet: bool = False,
+    g: float = 9.81,
+) -> float | npt.NDArray[np.float64]:
+
+    Rc_diml = calculate_dimensionless_crest_freeboard(
+        Hm0=Hm0,
+        q=q,
+        beta=beta,
+        gamma_beta=gamma_beta,
+        Hm0_swell=Hm0_swell,
+        c=c,
+        c_swell=c_swell,
+        short_crested_waves=short_crested_waves,
+        crossing_seas=crossing_seas,
+        parapet=parapet,
+        g=g,
+    )
+
+    Rc = Rc_diml * Hm0
+
+    return Rc
+
+
+def calculate_dimensionless_crest_freeboard(
+    Hm0: float | npt.NDArray[np.float64],
+    q: float | npt.NDArray[np.float64],
+    beta: float | npt.NDArray[np.float64] = np.nan,
+    gamma_beta: float | npt.NDArray[np.float64] = np.nan,
+    Hm0_swell: float | npt.NDArray[np.float64] = np.nan,
+    c: float = 1.0,
+    c_swell: float = 0.4,
+    short_crested_waves: bool = True,
+    crossing_seas: bool = False,
+    parapet: bool = False,
+    g: float = 9.81,
+) -> float | npt.NDArray[np.float64]:
+
+    a, b, c_beta, gamma_p = _determine_coefficients(
+        c=c,
+        short_crested_waves=short_crested_waves,
+        crossing_seas=crossing_seas,
+        parapet=parapet,
+    )
+
+    if not crossing_seas:
+        c_swell = 0.0
+    else:
+        if np.isnan(Hm0_swell):
+            raise ValueError(
+                "Hm0_swell must be provided when calculating wave overtopping for crossing seas."
+            )
+
+    if np.isnan(gamma_beta):
+        gamma_beta = calculate_influence_oblique_waves_gamma_beta(
+            beta=beta,
+            gamma_p=gamma_p,
+            c_beta=c_beta,
+        )
+
+    Rc_diml = (
+        np.power(
+            np.log((q / np.sqrt(g * np.power(Hm0, 3.0))) * (1.0 / a))
+            * (-gamma_beta * gamma_p / b),
+            1.0 / c,
+        )
+        + c_swell * Hm0_swell / Hm0
+    )
+
+    check_validity_range(
+        Hm0=Hm0,
+        beta=beta,
+        Rc=Rc_diml * Hm0,
+    )
+
+    return Rc_diml
+
+
+def _determine_coefficients(
+    c: float,
+    short_crested_waves,
+    crossing_seas,
+    parapet,
+):
+
     match c:
         case 1.0:
             if short_crested_waves | crossing_seas:
@@ -143,44 +287,4 @@ def calculate_dimensionless_overtopping_discharge_q(
                 "Invalid value for the exponent of the overtopping formula c. Supported values are 1.0 and 1.3."
             )
 
-    if not crossing_seas:
-        c_swell = 0.0
-    else:
-        if np.isnan(Hm0_swell):
-            raise ValueError(
-                "Hm0_swell must be provided when calculating wave overtopping for crossing seas."
-            )
-
-    if np.isnan(gamma_beta):
-        gamma_beta = calculate_influence_oblique_waves_gamma_beta(
-            beta=beta,
-            gamma_p=gamma_p,
-            c_beta=c_beta,
-        )
-
-    q_diml = a * np.exp(
-        -(b / (gamma_beta * gamma_p)) * np.power((Rc - c_swell * Hm0_swell) / Hm0, c)
-    )
-
-    check_validity_range(
-        Hm0=Hm0,
-        beta=beta,
-        Rc=Rc,
-        q_diml=q_diml,
-    )
-
-    return q_diml
-
-
-def calculate_influence_oblique_waves_gamma_beta(
-    beta: float | npt.NDArray[np.float64],
-    gamma_p: float,
-    c_beta: float,
-) -> float | npt.NDArray[np.float64]:
-    # """Calculate the influence factor for oblique wave incidence gamma_beta"""
-
-    gamma_beta = (1 - c_beta / gamma_p) * np.power(
-        np.cos(np.radians(beta)), 2
-    ) + c_beta / gamma_p
-
-    return gamma_beta
+    return a, b, c_beta, gamma_p
