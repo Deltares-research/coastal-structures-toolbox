42acce81422af4be8f7618f359b551048cc9e876
Joost den Bieman <jpdenbieman@gmail.com>
2025-07-30 11:57:57 +0200

    Update vangent2021_caisson_test.py

--- tests/hydraulic/wave_overtopping/vangent2021_caisson_test.py
+++ tests/hydraulic/wave_overtopping/vangent2021_caisson_test.py
@@ -115,54 +115,53 @@ def test_q_backward(
     assert q_calculated * 1000 == pytest.approx(q_expected, abs=1e-3)
 
 
-# @pytest.mark.parametrize(
-#     (
-#         "Hm0, Hm0_swell, Tmm10, beta, cot_alpha, q, Ac, B_berm, dh, gamma_f, gamma_v, Rc_expected"
-#     ),
-#     (
-#         ([2.0, 0.2, 5.00, 0.0, 3.0, 1.0e-3, 5.0, 0.0, 0.0, 1.0, 1.0, 5.916]),
-#         ([2.5, 0.2, 5.00, 0.0, 3.0, 1.0e-3, 5.0, 0.0, 0.0, 1.0, 1.0, 6.825]),
-#         ([2.0, 0.1, 5.00, 0.0, 3.0, 1.0e-3, 5.0, 0.0, 0.0, 1.0, 1.0, 5.876]),
-#         ([2.0, 0.2, 7.00, 0.0, 3.0, 1.0e-3, 5.0, 0.0, 0.0, 1.0, 1.0, 7.472]),
-#         ([2.0, 0.2, 5.00, 30.0, 3.0, 1.0e-3, 5.0, 0.0, 0.0, 1.0, 1.0, 4.967]),
-#         ([2.0, 0.2, 5.00, 0.0, 3.5, 1.0e-3, 5.0, 0.0, 0.0, 1.0, 1.0, 5.004]),
-#         ([2.0, 0.2, 5.00, 0.0, 3.0, 1.0e-3, 4.5, 1.0, 1.0, 1.0, 1.0, 5.544]),
-#         ([2.0, 0.2, 5.00, 0.0, 3.0, 1.0e-3, 5.0, 1.0, 1.0, 1.0, 1.0, 5.547]),
-#         ([2.0, 0.2, 5.00, 0.0, 3.0, 1.0e-3, 5.0, 2.0, 1.0, 1.0, 1.0, 5.009]),
-#         ([2.0, 0.2, 5.00, 0.0, 3.0, 1.0e-3, 5.0, 1.0, 0.8, 1.0, 1.0, 5.553]),
-#         ([2.0, 0.2, 5.00, 0.0, 3.0, 1.0e-3, 5.0, 0.0, 0.0, 0.45, 1.0, 2.706]),
-#         ([2.0, 0.2, 5.00, 0.0, 3.0, 1.0e-3, 5.0, 0.0, 0.0, 1.0, 0.8, 4.749]),
-#         ([2.0, 0.2, 5.00, 0.0, 3.5, 1.0e-3, 5.0, 2.0, 1.0, 1.0, 1.0, 4.239]),
-#         ([2.0, 0.2, 5.00, 30.0, 3.0, 1.0e-3, 5.0, 0.0, 0.0, 0.45, 1.0, 2.279]),
-#     ),
-# )
-# def test_Rc_backward(
-#     Hm0,
-#     Hm0_swell,
-#     Tmm10,
-#     beta,
-#     cot_alpha,
-#     q,
-#     Ac,
-#     B_berm,
-#     dh,
-#     gamma_f,
-#     gamma_v,
-#     Rc_expected,
-# ):
-#     Rc_calculated, _ = vangent2025.calculate_crest_freeboard_Rc(
-#         Hm0=Hm0,
-#         Hm0_swell=Hm0_swell,
-#         Tmm10=Tmm10,
-#         q=q,
-#         Ac=Ac,
-#         cot_alpha=cot_alpha,
-#         beta=beta,
-#         B_berm=B_berm,
-#         db=dh,
-#         gamma_f=gamma_f,
-#         gamma_v=gamma_v,
-#         design_calculation=False,
-#     )
+@pytest.mark.parametrize(
+    (
+        "Hm0, Hm0_swell, beta, q, c, short_crested_waves, crossing_seas, parapet, Rc_expected"
+    ),
+    (
+        ([2.0, 0.0, 0.0, 0.1033, 1.0, True, False, False, 5.00]),
+        ([2.5, 0.0, 0.0, 1.0146, 1.0, True, False, False, 5.00]),
+        ([2.0, 0.0, 60.0, 0.0109, 1.0, True, False, False, 5.00]),
+        ([2.0, 0.0, 0.0, 0.2738, 1.0, True, False, False, 4.50]),
+        ([2.0, 0.0, 0.0, 0.0884, 1.3, True, False, False, 5.00]),
+        ([2.5, 0.0, 0.0, 1.0389, 1.3, True, False, False, 5.00]),
+        ([2.0, 0.0, 60.0, 0.0076, 1.3, True, False, False, 5.00]),
+        ([2.0, 0.0, 0.0, 0.2609, 1.3, True, False, False, 4.50]),
+        ([2.0, 0.0, 0.0, 0.0309, 1.0, True, False, True, 5.00]),
+        ([2.5, 0.0, 0.0, 0.3869, 1.0, True, False, True, 5.00]),
+        ([2.0, 0.0, 60.0, 0.0072, 1.0, True, False, True, 5.00]),
+        ([2.0, 0.0, 0.0, 0.0926, 1.0, True, False, True, 4.50]),
+        ([2.0, 0.0, 0.0, 0.0804, 1.0, False, False, False, 5.00]),
+        ([2.5, 0.0, 0.0, 0.8307, 1.0, False, False, False, 5.00]),
+        ([2.0, 0.0, 60.0, 0.0138, 1.0, False, False, False, 5.00]),
+        ([2.0, 0.0, 0.0, 0.2187, 1.0, False, False, False, 4.50]),
+        ([2.0, 0.2, 0.0, 0.1207, 1.0, True, True, False, 5.00]),
+        ([2.5, 0.2, 0.0, 1.1494, 1.0, True, True, False, 5.00]),
+        ([2.0, 0.2, 60.0, 0.0132, 1.0, True, True, False, 5.00]),
+        ([2.0, 0.2, 0.0, 0.3201, 1.0, True, True, False, 4.50]),
+    ),
+)
+def test_Rc_backward(
+    Hm0,
+    Hm0_swell,
+    beta,
+    q,
+    c,
+    short_crested_waves,
+    crossing_seas,
+    parapet,
+    Rc_expected,
+):
+    Rc_calculated = vangent2021.calculate_crest_freeboard_Rc(
+        Hm0=Hm0,
+        Hm0_swell=Hm0_swell,
+        q=q / 1000,
+        beta=beta,
+        c=c,
+        short_crested_waves=short_crested_waves,
+        crossing_seas=crossing_seas,
+        parapet=parapet,
+    )
 
-#     assert Rc_calculated == pytest.approx(Rc_expected, abs=1e-2)
+    assert Rc_calculated == pytest.approx(Rc_expected, abs=1e-2)
