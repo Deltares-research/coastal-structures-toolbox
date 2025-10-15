72cf80cb68e9e657eb93ae2052c7d7ab3519956c
Joost den Bieman <jpdenbieman@gmail.com>
2025-10-15 15:33:09 +0200

    changes WIP

--- deltares_coastal_structures_toolbox/functions/structural/forces_crestwall/vangentvanderwerf2019.py
+++ deltares_coastal_structures_toolbox/functions/structural/forces_crestwall/vangentvanderwerf2019.py
@@ -5,7 +5,7 @@ import deltares_coastal_structures_toolbox.functions.core_utility as core_utilit
 import deltares_coastal_structures_toolbox.functions.hydraulic.wave_runup.vangent2001 as vangent2001
 
 
-def check_validity_FH2p(
+def check_validity(
     Rc: float | npt.NDArray[np.float64] = np.nan,
     Ac: float | npt.NDArray[np.float64] = np.nan,
     Hm0: float | npt.NDArray[np.float64] = np.nan,
@@ -51,82 +51,70 @@ def check_validity_FH2p(
     return
 
 
-def calculate_gamma_beta(
+def calculate_influence_oblique_waves_force_gamma_F_beta(
+    z2p: float | npt.NDArray[np.float64],
+    Ac: float | npt.NDArray[np.float64],
+    beta: float | npt.NDArray[np.float64],
+    gamma_A: float | npt.NDArray[np.float64],
+) -> float | npt.NDArray[np.float64]:
+
+    gamma_F_beta = (
+        (0.5 * np.power(np.cos(np.deg2rad(beta)), 2.0) + 0.5) * z2p - gamma_A * Ac
+    ) / (z2p - gamma_A * Ac)
+
+    return gamma_F_beta
+
+
+def calculate_influence_oblique_waves_horizontal_force_gamma_FH_beta(
+    z2p: float | npt.NDArray[np.float64],
+    Ac: float | npt.NDArray[np.float64],
+    beta: float | npt.NDArray[np.float64],
+    gamma_A: float | npt.NDArray[np.float64] = 1.0,
+) -> float | npt.NDArray[np.float64]:
+
+    gamma_FH_beta = calculate_influence_oblique_waves_force_gamma_F_beta(
+        z2p=z2p, Ac=Ac, beta=beta, gamma_A=gamma_A
+    )
+
+    return gamma_FH_beta
+
+
+def calculate_influence_oblique_waves_vertical_force_gamma_FV_beta(
+    z2p: float | npt.NDArray[np.float64],
+    Ac: float | npt.NDArray[np.float64],
+    beta: float | npt.NDArray[np.float64],
+    gamma_A: float | npt.NDArray[np.float64] = 0.75,
+) -> float | npt.NDArray[np.float64]:
+
+    gamma_FV_beta = calculate_influence_oblique_waves_force_gamma_F_beta(
+        z2p=z2p, Ac=Ac, beta=beta, gamma_A=gamma_A
+    )
+
+    return gamma_FV_beta
+
+
+def calculate_influence_oblique_waves_gamma_beta(
     beta: float | npt.NDArray[np.float64],
     c_beta: float | npt.NDArray[np.float64] = 0.5,
-) -> tuple[np.float64 | float | npt.NDArray[np.float64]]:
-    """Calculates influence factor for oblique incident waves on level
+) -> float | npt.NDArray[np.float64]:
+    """Calculates influence factor for oblique incident waves on wave runup level
 
     Parameters
     ----------
     beta : float | npt.NDArray[np.float64]
-        Incident wave angle in degrees measured wrt normal of the dike
+        Angle of wave incidence (degrees)
     c_beta : float | npt.NDArray[np.float64]
         Coefficient, 0.5 default
 
     Returns
     -------
-    tuple[np.float64 | float | npt.NDArray[np.float64], bool | npt.NDArray[np.bool]]
-        influence factor gamma_beta
+    float | npt.NDArray[np.float64]
+        influence factor for wave runup gamma_beta
     """
 
     gamma_beta = (1 - c_beta) * np.cos(beta) ** 2 + c_beta
 
     return gamma_beta
-
-
-def calculate_z2p(
-    Hm0: float | npt.NDArray[np.float64],
-    Tmm10: float | npt.NDArray[np.float64],
-    cot_alpha: float | npt.NDArray[np.float64],
-    gamma_beta: float | npt.NDArray[np.float64] = 1.0,
-    gamma_f: float | npt.NDArray[np.float64] = 0.45,
-    c0: float | npt.NDArray[np.float64] = 1.45,
-    c1: float | npt.NDArray[np.float64] = 5.0,
-):
-    """Calculate the wave runup height with the Van Gent (2001) formula, as referenced in the Van Gent
-    and van der Werf (2019) paper
-
-    For more details see Van Gent (2001), available here: https://doi.org/10.1061/(ASCE)0733-950X(2001)127:5(254)
-    or here: https://www.researchgate.net/publication/245293002_Wave_Run-Up_on_Dikes_with_Shallow_Foreshores
-    The coefficients c0 and c1 as well as gamma_f are according to the above paper and as used in the Van Gent
-    and van der Werf (2019) paper
-
-    For more details also see Van Gent and van der Werf (2019), available here:
-    https://doi.org/10.1016/j.coastaleng.2019.04.001
-    or here:
-    https://www.researchgate.net/publication/332744221_Influence_of_oblique_wave_attack_on_wave_overtopping_and_forces_on_rubble_mound_breakwater_crest_walls
-
-    Parameters
-    ----------
-    Hm0 : float | npt.NDArray[np.float64]
-        Spectral significant wave height (m)
-    Tmm10 : float | npt.NDArray[np.float64]
-        _description_Spectral wave period Tm-1,0 (s)
-    cot_alpha : _tyfloat | npt.NDArray[np.float64]pe_
-        Cotangent of the front-side slope of the structure (-)
-    gamma_f : float | npt.NDArray[np.float64], optional
-        Influence factor for roughness, by default 0.45
-    gamma_beta : float | npt.NDArray[np.float64], optional
-        Incluence factor of obliquely incident waves, by default 1
-    c0 : float | npt.NDArray[np.float64], optional
-        Coefficient in wave runup formula (-), by default 1.45 (short waves, Hm0 & Tmm10)
-    c1 : float | npt.NDArray[np.float64], optional
-        Coefficient in wave runup formula (-), by default 5.0  (short waves, Hm0 & Tmm10)
-
-    Returns
-    -------
-    float | npt.NDArray[np.float64]
-        The 2% exceedance wave runup height z2% (m)
-    """
-
-    gamma = gamma_f * gamma_beta
-
-    ru2p = vangent2001.calculate_wave_runup_height_zXp(
-        H=Hm0, Tmm10=Tmm10, cot_alpha=cot_alpha, gamma=gamma, c0=c0, c1=c1
-    )
-
-    return ru2p
 
 
 def calculate_FH2p_perpendicular(
@@ -185,13 +173,15 @@ def calculate_FH2p_perpendicular(
 
     gamma = gamma_f * gamma_beta
 
-    ru2p = vangent2001.calculate_wave_runup_height_zXp(
+    z2p = vangent2001.calculate_wave_runup_height_zXp(
         H=Hm0, Tmm10=Tmm10, cot_alpha=cot_alpha, gamma=gamma, c0=c0, c1=c1
     )
 
     FH2p = calculate_FH2p_perpendicular_from_z2p(
-        Hm0=Hm0, z2p=ru2p, rho_water=rho_water, Ac=Ac, Rc=Rc, Hwall=Hwall, g=g, cFH=cFH
+        Hm0=Hm0, z2p=z2p, rho_water=rho_water, Ac=Ac, Rc=Rc, Hwall=Hwall, g=g, cFH=cFH
     )
+
+    check_validity(Hm0=Hm0, Ac=Ac, Rc=Rc)
 
     return FH2p
 
@@ -207,14 +197,53 @@ def calculate_FH2p_perpendicular_from_z2p(
     cFH: float | npt.NDArray[np.float64] = 1.0,
 ) -> float | npt.NDArray[np.float64]:
 
-    check_validity_FH2p(Hm0=Hm0, Ac=Ac, Rc=Rc)
+    check_validity(Hm0=Hm0, Ac=Ac, Rc=Rc)
 
     # Formula 13 paper
     FH2p = cFH * rho_water * g * Hwall * (z2p - Ac)
 
     FH2p = np.min(FH2p, 0)
 
+    check_validity(Hm0=Hm0, Ac=Ac, Rc=Rc)
+
     return FH2p
+
+
+def calculate_FH2p_oblique(
+    Hm0: float | npt.NDArray[np.float64],
+    Tmm10: float | npt.NDArray[np.float64],
+    cot_alpha: float | npt.NDArray[np.float64],
+    Ac: float | npt.NDArray[np.float64],
+    Rc: float | npt.NDArray[np.float64],
+    Hwall: float | npt.NDArray[np.float64],
+    gamma_beta: float | npt.NDArray[np.float64] = 1.0,
+    gamma_f: float | npt.NDArray[np.float64] = 0.45,
+    c0: float | npt.NDArray[np.float64] = 1.45,
+    c1: float | npt.NDArray[np.float64] = 5.0,
+    cFH: float | npt.NDArray[np.float64] = 1.0,
+    rho_water: float | npt.NDArray[np.float64] = 1025.0,
+    g: float | npt.NDArray[np.float64] = 9.81,
+) -> float | npt.NDArray[np.float64]:
+
+    gamma = gamma_f * gamma_beta
+
+    z2p = vangent2001.calculate_wave_runup_height_zXp(
+        H=Hm0, Tmm10=Tmm10, cot_alpha=cot_alpha, gamma=gamma, c0=c0, c1=c1
+    )
+
+    FH2p_perpendicular = calculate_FH2p_perpendicular_from_z2p(
+        Hm0=Hm0, z2p=z2p, rho_water=rho_water, Ac=Ac, Rc=Rc, Hwall=Hwall, g=g, cFH=cFH
+    )
+
+    gamma_FH_beta = calculate_influence_oblique_waves_horizontal_force_gamma_FH_beta(
+        z2p=z2p, Ac=Ac, beta=cot_alpha
+    )
+
+    FH2p_oblique = gamma_FH_beta * FH2p_perpendicular
+
+    check_validity(Hm0=Hm0, Ac=Ac, Rc=Rc)
+
+    return FH2p_oblique
 
 
 def calculate_FV2p_perpendicular(
@@ -222,16 +251,54 @@ def calculate_FV2p_perpendicular(
     rho_water: float | npt.NDArray[np.float64],
     Ac: float | npt.NDArray[np.float64],
     Bwall: float | npt.NDArray[np.float64],
-    CFV: float | npt.NDArray[np.float64],
     Fb: float | npt.NDArray[np.float64],  # Level base plate relative to water level [m]
+    cFV: float | npt.NDArray[np.float64] = 0.4,
+    cFb: float | npt.NDArray[np.float64] = 0.5,
     g: float | npt.NDArray[np.float64] = 9.81,  # gravitational acceleration [m2/s]
 ) -> float | npt.NDArray[np.float64]:
 
-    FV2p = CFV * rho_water * g * Bwall * (z2p - 0.75 * Ac) * (1 - (np.sqrt(Fb / Ac)))
+    FV2p = (
+        cFV * rho_water * g * Bwall * (z2p - 0.75 * Ac) * (1 - (np.power(Fb / Ac, cFb)))
+    )
 
     FV2p = np.min(FV2p, 0)
 
     return FV2p
+
+
+def calculate_FV2p_oblique(
+    Ac: float | npt.NDArray[np.float64],
+    Bwall: float | npt.NDArray[np.float64],
+    Fb: float | npt.NDArray[np.float64],
+    Hm0: float | npt.NDArray[np.float64],
+    Tmm10: float | npt.NDArray[np.float64],
+    cot_alpha: float | npt.NDArray[np.float64],
+    gamma_beta: float | npt.NDArray[np.float64] = 1.0,
+    gamma_f: float | npt.NDArray[np.float64] = 0.45,
+    c0: float | npt.NDArray[np.float64] = 1.45,
+    c1: float | npt.NDArray[np.float64] = 5.0,
+    cFV: float | npt.NDArray[np.float64] = 0.4,
+    rho_water: float | npt.NDArray[np.float64] = 1025.0,
+    g: float | npt.NDArray[np.float64] = 9.81,
+) -> float | npt.NDArray[np.float64]:
+
+    gamma = gamma_f * gamma_beta
+
+    z2p = vangent2001.calculate_wave_runup_height_zXp(
+        H=Hm0, Tmm10=Tmm10, cot_alpha=cot_alpha, gamma=gamma, c0=c0, c1=c1
+    )
+
+    FV2p_perpendicular = calculate_FV2p_perpendicular(
+        z2p=z2p, rho_water=rho_water, Ac=Ac, Bwall=Bwall, cFV=cFV, Fb=Fb, g=g
+    )
+
+    gamma_FV_beta = calculate_influence_oblique_waves_vertical_force_gamma_FV_beta(
+        z2p=z2p, Ac=Ac, beta=cot_alpha
+    )
+
+    FV2p_oblique = gamma_FV_beta * FV2p_perpendicular
+
+    return FV2p_oblique
 
 
 def calculate_FH01p_perpendicular(
@@ -251,7 +318,3 @@ def calculate_FV01p_perpendicular(
     FV01p = (2.88 - 32 * sop) * FV2p
 
     return FV01p
-
-
-# TODO gammas laten uitrekenen (1 main functie, uitsplitsen naar H en V)
-# TODO oblique krachten uitrekenen
